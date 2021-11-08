1d83af46b716f0616053cb4183927087f9821018
josemoncada87 <josemoncada87@hotmail.com>
2021-11-08 08:53:42 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+'use strict'
+
+console.log("Llamado global:");
+console.log(this);
+
+function funcionGlobal() {
+    console.log("Llamado función global:");
+    console.log(this);
+}
+funcionGlobal();
+
+const expGlobal = function () {
+    console.log("Llamado expresión global:");
+    console.log(this);
+}
+expGlobal();
+
+function GeneradorFunciones() {
+    return function () {
+        console.log("Llamado generadorFunciones:");
+        console.log(this);
+    }
+}
+const funcionGenerada = new GeneradorFunciones();
+funcionGenerada();
+
+
+function FabricaObjetos() {
+    return {
+        mostrar: function () {
+            console.log("Llamado FabricaObjetos:");
+            console.log(this);
+        }
+    }
+}
+const objetoFabricado = new FabricaObjetos();
+objetoFabricado.mostrar();
+
+class Prueba{
+    constructor(){
+        console.log("Desde constructor:");
+        console.log(this);
+    }
+    mostrar(){
+        console.log("Desde método de clase:");
+        console.log(this);
+    }
+}
+let obj = new Prueba();
+obj.mostrar();
+
+let litObj = {
+    mostrar:function(){
+    console.log("Desde objeto literal:");
+    console.log(this);
+    }
+}
+litObj.mostrar();
