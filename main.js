a66f37724a727c49fe22e8af5b6989d71c965892
josemoncada87 <josemoncada87@hotmail.com>
2021-11-08 08:54:41 -0500

    ejercicio

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-'use strict'
+//'use strict'
+
+/*
+Ejercicio usando this en contexto
+Estrictos y descuidados
+
+1. Ejecute el código y observe la consola.
+2. Active el 'use strict' y ejecutelo de nuevo
+3. ¿Qué ha cambiado?
+4. ¿Qué se puede decir del cambio?
+5. ¿Qué podría inferirse o generalizarse del comportamiento del this?
+*/
 
 console.log("Llamado global:");
 console.log(this);
